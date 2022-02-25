300b25ee42c9ba3cc093b93f17ddb606f0d8cd49
matt-ward <matthewottoward@gmail.com>
2022-02-25 14:45:37 -0500

    added initial js and html files

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const { Pool } = require('pg');
+const cors = require('cors');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+
+const pool = new Pool();
+
+app.get('/location', (req, res) => {
+    res.send('Hello, world!');
+});
---
+++ my-location.js
@@ -0,0 +1,33 @@
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  const coords = document.getElementById('status'); 
+  
+  function success(pos) {
+    var crd = pos.coords;
+    var long = crd.longitude;
+    var lat = crd.latitude;
+    // console.log(crd.longitude, crd.latitude);
+    fetch(`https://powerful-woodland-32319.herokuapp.com/location?long=${long}&lat=${lat}`)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        //in here I need to make sure the json is getting printed to the html element
+        // coords.innerHTML = json[0].address;
+        for(const each of json){
+          let li = document.createElement('li');
+          li.innerHTML = each.address;
+          coords.appendChild(li);
+        }
+        console.log('finished');
+      });
+  }
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error, [options]);
