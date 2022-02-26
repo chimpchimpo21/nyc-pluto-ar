3f15cfada458cc7f5d1eda553a9700082db0c007
matt-ward <matthewottoward@gmail.com>
2022-02-26 10:19:58 -0500

    trying heroku connection method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { Pool, Client } = require('pg');
-const connectionString = 'postgres://ncxtnyffvzymth:95e455939a032eed3410b2fd00c303114b662b257374443010e8e7627b4c37f1@ec2-18-235-4-83.compute-1.amazonaws.com:5432/dd48po6c8ohfo4'
+// const connectionString = 'postgres://ncxtnyffvzymth:95e455939a032eed3410b2fd00c303114b662b257374443010e8e7627b4c37f1@ec2-18-235-4-83.compute-1.amazonaws.com:5432/dd48po6c8ohfo4'
 const cors = require('cors');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
@@ -25,12 +25,16 @@ app
         var values = [long, lat];
         //console.log(req.query);
         const text = 'SELECT address, ST_X(ST_Transform(geom_sm, 4326)), ST_Y(ST_Transform(geom_sm, 4326)) FROM nyc_pluto_centroids WHERE ST_Intersects(ST_Buffer(ST_Transform(ST_SetSRID(ST_Point($1, $2), 4326), 3857), 50), geom_sm);';
-        const { Client } = require('pg');
+        // const { Client } = require('pg');
         const { rows } = require('pg/lib/defaults');
 
         const client = new Client({
-            connectionString,
-        })
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
+        
         client.connect();
 
         client.query(text, values, (err, result) => {
