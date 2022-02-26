e952d5d16bd0a6f057e48e01fdd783f49cda4011
matt-ward <matthewottoward@gmail.com>
2022-02-26 10:51:53 -0500

    modified index.js

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const { Client } = require('pg');
 
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    ssl: process.env.DATABASE_URL ? true : false
 });
 
 client.connect();
