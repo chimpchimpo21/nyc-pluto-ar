19d0a92abf8044083c045d6ad8067a3fd83813b7
matt-ward <matthewottoward@gmail.com>
2022-02-26 10:31:06 -0500

    modified index.js

--- index.js
+++ index.js
@@ -7,52 +7,51 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 
+// app.get('/', (req, res) => {
+//     console.log(req.query);
+//     return res.send("hello, world!");
+// })
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/find-centroids.html'));
+// })
+// app.get('/my-location.js', (req, res) => {
+//         res.sendFile(path.join(__dirname + '/my-location.js'));
+// })
 
-app
-    // .get('/', (req, res) => {
-    //     console.log(req.query);
-    //     return res.send("hello, world!");
-    // })
-    .get('/', (req, res) => {
-        res.sendFile(path.join(__dirname + '/find-centroids.html'));
-    })
-app.get('/my-location.js', (req, res) => {
-        res.sendFile(path.join(__dirname + '/my-location.js'));
-    })
-app.get('/api', (req, res) => {
-        let long = req.query.long;
-        let lat = req.query.lat;
-        var values = [long, lat];
-        //console.log(req.query);
-        const text = 'SELECT address, ST_X(ST_Transform(geom_sm, 4326)), ST_Y(ST_Transform(geom_sm, 4326)) FROM nyc_pluto_centroids WHERE ST_Intersects(ST_Buffer(ST_Transform(ST_SetSRID(ST_Point($1, $2), 4326), 3857), 50), geom_sm);';
-        // const { Client } = require('pg');
-        const { rows } = require('pg/lib/defaults');
+// app.get('/api', (req, res) => {
+//         let long = req.query.long;
+//         let lat = req.query.lat;
+//         var values = [long, lat];
+//         //console.log(req.query);
+//         const text = 'SELECT address, ST_X(ST_Transform(geom_sm, 4326)), ST_Y(ST_Transform(geom_sm, 4326)) FROM nyc_pluto_centroids WHERE ST_Intersects(ST_Buffer(ST_Transform(ST_SetSRID(ST_Point($1, $2), 4326), 3857), 50), geom_sm);';
+//         // const { Client } = require('pg');
+//         const { rows } = require('pg/lib/defaults');
 
-        const client = new Client({
-            connectionString: process.env.DATABASE_URL,
-            ssl: {
-                rejectUnauthorized: false
-            }
-        });
+//         const client = new Client({
+//             connectionString: process.env.DATABASE_URL,
+//             ssl: {
+//                 rejectUnauthorized: false
+//             }
+//         });
 
-        client.connect();
+//         client.connect();
 
-        client.query(text, values, (err, result) => {
-            if(!err) {
-                // for (var i = 0; i < result.rows.length; i++) {
-                //     let long = result.rows[i].st_x
-                //     let lat = result.rows[i].st_y
-                //     res.send(`longitude: ${long} ; latitude: ${lat}`);
-                // }
-                // console.log(res.rows);
-                console.log("request made");
-                res.send(result.rows);
+//         client.query(text, values, (err, result) => {
+//             if(!err) {
+//                 // for (var i = 0; i < result.rows.length; i++) {
+//                 //     let long = result.rows[i].st_x
+//                 //     let lat = result.rows[i].st_y
+//                 //     res.send(`longitude: ${long} ; latitude: ${lat}`);
+//                 // }
+//                 // console.log(res.rows);
+//                 console.log("request made");
+//                 res.send(result.rows);
         
-            }
-            else {
-                console.log(err.message);
-            }
-            client.end;
-        });
-    })
+//             }
+//             else {
+//                 console.log(err.message);
+//             }
+//             client.end;
+//         });
+// })
 app.listen(PORT, () => console.log(`listening on ${ PORT }`));
