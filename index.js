eb0a9688abc377a9402a91682b0e1360566a2043
matt-ward <matthewottoward@gmail.com>
2022-03-10 08:17:18 -0500

    touch event issue

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get('/sunnyside', async (req, res) => {
         let lat = req.query.lat;
         try {
             const client = await pool.connect();
-            const result = await client.query(`SELECT address, ST_X(geom_sm), ST_Y(geom_sm) FROM centroids_wgs84_pluto_local WHERE ST_Intersects(ST_Buffer(ST_Transform(ST_SetSRID(ST_Point(${long}, ${lat}), 4326), 3857), 50), geom_sm);`);
+            const result = await client.query(`SELECT address, ST_X(geom_sm), ST_Y(geom_sm) FROM centroids_wgs84_pluto_local WHERE ST_Intersects(ST_Buffer(ST_Transform(ST_SetSRID(ST_Point(${long}, ${lat}), 4326), 3857), 30), geom_sm);`);
             // const results = { 'results': (result) ? result.rows : null};
             // res.render('pages/db', results);
             res.send(result.rows);
