caa4c9e597e791be952df15b9d930f6650cfabb3
chimpchimpo21 <77162592+chimpchimpo21@users.noreply.github.com>
2022-03-22 17:03:08 -0400

    remove unnecessary folder

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const pool = new Pool({
 
 app.use(cors())
 app.use(express.static('public'))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+// app.set('views', path.join(__dirname, 'views'))
+// app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname + '/index.html'));
     })
