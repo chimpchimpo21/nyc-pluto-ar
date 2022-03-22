8f137536c4d4cac651defd5886d1bd5d808a45e6
chimpchimpo21 <77162592+chimpchimpo21@users.noreply.github.com>
2022-03-22 15:59:29 -0400

    delete outer folder

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 // });
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:admin@localhost:5432/local_centroids',
+    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:admin@localhost:5432/thesis',
     // ssl: process.env.DATABASE_URL ? true : false
     ssl: { rejectUnauthorized: false }
 });
@@ -23,14 +23,14 @@ app.use(express.static('public'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname + '/public/find-centroids.html'));
+        res.sendFile(path.join(__dirname + '/find-centroids.html'));
     })
 app.get('/db', async (req, res) => {
         let long = req.query.long;
         let lat = req.query.lat;
         try {
             const client = await pool.connect();
-            const result = await client.query(`SELECT address, assesstot, bldgclass, exempttot, numfloors, ST_X(geom_sm), ST_Y(geom_sm) FROM hunter WHERE ST_Intersects(ST_Buffer(ST_Transform(ST_SetSRID(ST_Point(${long}, ${lat}), 4326), 3857), 50), geom_sm);`);
+            const result = await client.query(`SELECT *, ST_X(wkb_geometry), ST_Y(wkb_geometry) FROM nyc_pluto_final WHERE ST_Intersects(ST_Buffer(ST_Transform(ST_SetSRID(ST_Point(${long}, ${lat}), 4326), 3857), 50), wkb_geometry);`);
             // const results = { 'results': (result) ? result.rows : null};
             // res.render('pages/db', results);
             res.send(result.rows);
@@ -45,7 +45,7 @@ app.get('/sunnyside', async (req, res) => {
         let lat = req.query.lat;
         try {
             const client = await pool.connect();
-            const result = await client.query(`SELECT address, ST_X(geom_sm), ST_Y(geom_sm) FROM centroids_wgs84_pluto_local WHERE ST_Intersects(ST_Buffer(ST_Transform(ST_SetSRID(ST_Point(${long}, ${lat}), 4326), 3857), 30), geom_sm);`);
+            const result = await client.query(`SELECT *, ST_X(geom_sm), ST_Y(geom_sm) FROM centroids_wgs84_pluto_local WHERE ST_Intersects(ST_Buffer(ST_Transform(ST_SetSRID(ST_Point(${long}, ${lat}), 4326), 3857), 30), geom_sm);`);
             // const results = { 'results': (result) ? result.rows : null};
             // res.render('pages/db', results);
             res.send(result.rows);
