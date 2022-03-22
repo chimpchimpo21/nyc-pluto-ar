f85512dfe9c1e0b822e8278bdafeef0227209780
chimpchimpo21 <77162592+chimpchimpo21@users.noreply.github.com>
2022-03-22 16:53:54 -0400

    remove local db

--- index.js
+++ index.js
@@ -40,19 +40,4 @@ app.get('/db', async (req, res) => {
             res.send("Error " + err);
         }
 })
-app.get('/sunnyside', async (req, res) => {
-        let long = req.query.long;
-        let lat = req.query.lat;
-        try {
-            const client = await pool.connect();
-            const result = await client.query(`SELECT *, ST_X(geom_sm), ST_Y(geom_sm) FROM centroids_wgs84_pluto_local WHERE ST_Intersects(ST_Buffer(ST_Transform(ST_SetSRID(ST_Point(${long}, ${lat}), 4326), 3857), 30), geom_sm);`);
-            // const results = { 'results': (result) ? result.rows : null};
-            // res.render('pages/db', results);
-            res.send(result.rows);
-            client.release();
-        } catch (err) {
-            console.error(err);
-            res.send("Error " + err);
-        }
-})
 app.listen(PORT, () => console.log(`listening on ${ PORT }`));
