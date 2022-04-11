bec83e5ddcd9be34c0fed344b84734873dce59b7
chimpchimpo21 <77162592+chimpchimpo21@users.noreply.github.com>
2022-04-11 12:36:26 -0400

    db testing

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/db', async (req, res) => {
         let lat = req.query.lat;
         try {
             const client = await pool.connect();
-            const result = await client.query(`SELECT *, ST_X(wkb_geometry), ST_Y(wkb_geometry) FROM nyc_pluto_final WHERE ST_Intersects(ST_Buffer(ST_Transform(ST_SetSRID(ST_Point(${long}, ${lat}), 4326), 3857), 50), wkb_geometry);`);
+            const result = await client.query(`SELECT *, ST_X(wkb_geometry), ST_Y(wkb_geometry) FROM nyc_pluto_final WHERE ST_Intersects(ST_Buffer(ST_Transform(ST_SetSRID(ST_Point(${long}, ${lat}), 4326), 3857), 500), wkb_geometry);`);
             res.send(result.rows);
             client.release();
         } catch (err) {
